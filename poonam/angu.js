d0e02736a10ea40e60e16f246af66ed1d199b79c
poonam123rajak <poonam123rajak@gmail.com>
2017-04-20 13:54:44 +0530

    Add files via upload

---
+++ poonam/angu.js
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js">
+</script>
+<body>
+    <div ng-app="">
+        <p>Input something</p>
+        <p>Name:<input type="text" ng-model="name" placeholder="ente u r name">
+            </p>
+<h1>hello{{name}}</h1>
+    </div>
+</body>
+</html>
---
+++ poonam/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+<body>
+
+
+</body>
+</html>
+
+
+
+
+
+
+<?php
+    
+    
+    ?>
